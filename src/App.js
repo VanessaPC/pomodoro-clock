bca05a6630562b8ee6c6641de79a46f8ff7841de
Nessa <rc.vanessa87@gmail.com>
2017-09-10 10:51:19 +0100

    finsihed app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { Timer } from './Timer.js';
-import './App.css';
 
 // show the countdown on the input 
 // if the user enters a negative number, reset it to 0
@@ -9,11 +8,9 @@ import './App.css';
 class App extends Component {
     render() {
         return (
-            <div className="container">
-                <div className="row">
+
                    <Timer />
-                </div>
-            </div>
+
         );
     }
 }
--- src/Timer.js
+++ src/Timer.js
@@ -1,177 +1,261 @@
 import React, { Component } from 'react';
-import { render } from 'react-dom';
 import moment from 'moment';
 import 'moment-duration-format';
 import './Timer.css';
 
 
 export class Timer extends Component {
-  constructor (){
+  constructor() {
     super();
 
     this.state = {
-          name: '',
-          maxTime : 0,
-          date: moment().format('dddd, MMMM Do YYYY, h:mm:ss a'),
-          time: moment.duration(0),
-          workingTime: moment.duration(0),
-          breakTime: moment.duration(0)
-      }
+      name: "",
+      maxTime: 0,
+      date: moment().format("dddd, MMMM Do YYYY, h:mm:ss a"),
+      time: moment.duration(0),
+      workingTime: moment.duration(0),
+      breakTime: moment.duration(0)
+    };
 
-     this.onAddition = this.onAddition.bind(this);
-     this.onSubstraction = this.onSubstraction.bind(this);
-     this.onStart = this.onStart.bind(this);
-     this.onStop = this.onStop.bind(this);
-     this.onPause = this.onPause.bind(this);
-     this.onSave = this.onSave.bind(this);
-     this.onClear = this.onClear.bind(this);
+    this.onAdditionWork = this.onAdditionWork.bind(this);
+    this.onSubstractionWork = this.onSubstractionWork.bind(this);
+    this.onAdditionBreak = this.onAdditionBreak.bind(this);
+    this.onSubstractionBreak = this.onSubstractionBreak.bind(this);
+    this.onStart = this.onStart.bind(this);
+    this.onStop = this.onStop.bind(this);
+    this.onPause = this.onPause.bind(this);
+    this.onClear = this.onClear.bind(this);
 
-     this.interval;
-  }
-  
-  // I want to keep a function that adds to the number that is being displayed 
-  // on the timer 
-  onAddition(e) {
-    let timer = this.state.time;
-
-    timer = timer.add(parseInt(e.target.value), 'minutes');
-    console.log(timer.format('H:mm:ss'));
-
-    this.setState({time: timer});
-
+    this.interval;
   }
 
-  // I want to keep a function that substracts from the number displayed on the timer 
-  onSubstraction(e) {
-  let timer = this.state.time;
-  timer = timer.subtract(parseInt(e.target.value), 'minutes');
-  this.setState({time: timer});
+  // I want to keep a function that adds to the number that is being displayed
+  // on the timer
+  onAdditionWork(e) {
+    let timer = this.state.workingTime;
 
+    timer = timer.add(parseInt(e.target.value, 10), "minutes");
+    console.log(timer.format("H:mm:ss"));
+
+    this.setState({ workingTime: timer });
   }
 
-    // I want to start a count down fromt he Max Time we calculated the user wanted and count down to 0.  
+  // I want to keep a function that substracts from the number displayed on the timer
+  onSubstractionWork(e) {
+    let timer = this.state.workingTime;
+    timer = timer.subtract(parseInt(e.target.value, 10), "minutes");
+    this.setState({ workingTime: timer });
+  }
+
+  // I want to keep a function that adds to the number that is being displayed
+  // on the timer
+  onAdditionBreak(e) {
+    let timer = this.state.breakTime;
+
+    timer = timer.add(parseInt(e.target.value, 10), "minutes");
+    console.log(timer.format("H:mm:ss"));
+
+    this.setState({ breakTime: timer });
+  }
+
+  // I want to keep a function that substracts from the number displayed on the timer
+  onSubstractionBreak(e) {
+    let timer = this.state.breakTime;
+    timer = timer.subtract(parseInt(e.target.value, 10), "minutes");
+    this.setState({ breakTime: timer });
+  }
+
+  // I want to start a count down fromt he Max Time we calculated the user wanted and count down to 0.
   onStart() {
-     this.interval = window.setInterval( (time) => {
-        let timer = this.state.time;
-        if (this.state.workingTime.as('seconds') !== 0) {
-            let timer = this.state.workingTime;
-        } else {
-            let timer = this.state.breakTime;
-        }
-      let seconds = moment.duration(timer, 'HH:mm:ss').asSeconds();
-        
-          if (seconds > 0){
-            timer = timer.subtract(1, 'seconds');
-            seconds --;
-            this.setState({time: timer});  
-
-          } else if (seconds === 0) {
-            clearInterval(this.interval);
-          }     
-       
-      }, 1000);
+    let timer;
+    this.interval = window.setInterval(time => {
+      if (this.state.workingTime.as("seconds") !== 0) {
+        timer = this.state.workingTime;
+      } else {
+        timer = this.state.breakTime;
+      }
+      if (this.state.workingTime.as("seconds") < 0) {
+        this.setState({
+          time: moment.duration(0),
+          workingTime: moment.duration(0)
+        });
+      } else if (this.state.breakTime.as("seconds") < 0) {
+        this.setState({
+          time: moment.duration(0),
+          breakTime: moment.duration(0)
+        });
+      }
+      // let timer = this.state.time;
+      let seconds = moment.duration(timer, "HH:mm:ss").asSeconds();
+      console.log("here");
+      if (seconds > 0) {
+        timer = timer.subtract(1, "seconds");
+        seconds--;
+        this.setState({ time: timer });
+      } else if (seconds <= 0) {
+        clearInterval(this.interval);
+      }
+    }, 1000);
   }
 
-  // On finish I want to reset the counter, and values, to allow the user to start again 
+  // On finish I want to reset the counter, and values, to allow the user to start again
   onStop() {
     clearInterval(this.interval);
     this.setState({
       time: moment.duration(0)
-    })
+    });
   }
 
-
-// When the user wants to pause the action, it should pause but should not clear the interval 
+  // When the user wants to pause the action, it should pause but should not clear the interval
   onPause() {
     clearInterval(this.interval);
-    
-  }
-
-// creating a task to go through 
-  onSave() {
-    let lapse = this.state.time;
-    // I want the time to be rest, after working time is updated, 
-
-    if (this.state.workingTime.as('seconds') === 0) {
-      this.setState({
-        time: moment.duration(0),
-        workingTime: lapse
-      });
-    } else {
-      this.setState({
-        time: moment.duration(0),
-        breakTime: lapse
-      })
-    }
-   
   }
 
   onClear() {
     this.setState({
-      name: '',
-      maxTime : 0,
-      date: moment().format('dddd, MMMM Do YYYY, h:mm:ss a'),
+      name: "",
+      maxTime: 0,
+      date: moment().format("dddd, MMMM Do YYYY, h:mm:ss a"),
       time: moment.duration(0),
       workingTime: moment.duration(0),
       breakTime: moment.duration(0)
-    })
+    });
     clearInterval(this.interval);
-
   }
 
-  
-
   render() {
-    const timer = this.state.time.as('seconds');
-    let workTime = this.state.workingTime.format('H:mm:ss', ({trim:false}));
-    let breakT = this.state.breakTime.format('H:mm:ss', ({trim:false}));
+    const timer = this.state.time.as("seconds");
+    let workTime = this.state.workingTime.format("H:mm:ss", { trim: false });
+    let breakT = this.state.breakTime.format("H:mm:ss", { trim: false });
     let timing;
 
     if (timer > 30) {
-      timing = this.state.time.format('H:mm:ss', {trim:false});
+      timing = this.state.time.format("H:mm:ss", { trim: false });
     } else {
-      timing = this.state.time.format('ss')+' seconds';
+      timing = this.state.time.format("ss") + " seconds";
     }
 
     return (
       <div>
-      <p> {this.state.date}
-      </p>
-      
-     
-      <div>
-        <p> {timing} </p>
-      </div>
-       <button onClick={this.onStart} name="start" >
-                 Start 
-          </button> 
-          <button onClick={this.onStop} name="stop" >
-                 Stop 
-          </button> 
-          <button onClick={this.onPause} name="pause" >
-                 Pause 
-          </button> 
-       
-          <button onClick={this.onAddition} name="+" value="5"> +
-          </button>
-          <div> 5 </div>
-          <button onClick={this.onSubstraction} name="-" value="5"> -
-          </button>
-
-          <button onClick={this.onAddition} name="+" value="25"> +
-          </button>
-          <div> 25 </div>
-          <button onClick={this.onSubstraction} name="-" value="25"> -
-          </button>
-          <button onClick={this.onSave} name="save"> Save </button>
-          <button onClick={this.onClear} name="clear"> Clear </button>
-
-          <div> this is the working time 
-            <p> {workTime} </p>
+        <div className="container">
+          <div className="date-display">
+            <h3>
+              Today is {" "}{this.state.date}
+            </h3>
           </div>
-          <div> this is the break time 
-            <p> {breakT} </p>
+        </div>
+        <div className="main-container-controls">
+          <div className=" left controls-container">
+            <div className="text-reading">
+              {" "}Active time
+              <p className="number"> {workTime} </p>
+            </div>
+            <button
+              onClick={this.onAdditionWork}
+              name="+"
+              value="5"
+              className="text calc-button one"
+            >
+              {" "}+
+            </button>
+            <div className="number calc-number two"> 5 </div>
+            <button
+              onClick={this.onSubstractionWork}
+              name="-"
+              value="5"
+              className="text calc-button three"
+            >
+              {" "}-
+            </button>
+            <button
+              onClick={this.onSubstractionWork}
+              name="-"
+              value="25"
+              className="text calc-button four"
+            >
+              {" "}-
+            </button>
+            
+            <div className="number calc-number five"> 25 </div>
+            <button
+              onClick={this.onAdditionWork}
+              name="+"
+              value="25"
+              className="text calc-button six"
+            >
+              {" "}+
+            </button>
+
           </div>
+          <div className="right controls-container">
+            <div className="text-reading">
+              {" "}Break
+              <p className="number"> {breakT} </p>
+            </div>
+            <button
+              onClick={this.onAdditionBreak}
+              name="+"
+              value="5"
+              className="text calc-button one"
+            >
+              {" "}+
+            </button>
+            <div className="number calc-number two"> 5 </div>
+            <button
+              onClick={this.onSubstractionBreak}
+              name="-"
+              value="5"
+              className="text calc-button three"
+            >
+              {" "}-
+            </button>
+            <button
+              onClick={this.onSubstractionBreak}
+              name="-"
+              value="25"
+              className="text calc-button four"
+            >
+              {" "}-
+            </button>
+            <div className="number calc-number five"> 25 </div>
+            <button
+              onClick={this.onAdditionBreak}
+              name="+"
+              value="25"
+              className="text calc-button six"
+            >
+              {" "}+
+            </button>
+            
+          </div>
+        </div>
+        <div className="control-buttons space-top">
+          <button onClick={this.onClear} name="clear" className="text">
+            {" "}Clear times
+            {" "}
+          </button>
+        </div>
+        <div className="container">
+          <div className="counter-display">
+            <div className="circle">
+              <p className="number-timer"> {timing} </p>
+            </div>
+          </div>
+          <div className="control-buttons">
+            <button onClick={this.onStart} name="start" className="text">
+              Start
+              {" "}
+            </button>
+            <button onClick={this.onStop} name="stop" className="text">
+              Stop
+              {" "}
+            </button>
+            <button onClick={this.onPause} name="pause" className="text">
+              Pause
+              {" "}
+            </button>
+          </div>
+        </div>
       </div>
     );
   }
